351ce1fe796ea08e6db1138865a2030da3c88b74
AMAN RAJ <amanraj7166@gmail.com>
2022-08-28 16:09:21 +0530

    Time: 88 ms (78.76%), Space: 5.9 MB (81.46%) - LeetHub

--- 526-beautiful-arrangement/526-beautiful-arrangement.cpp
+++ 526-beautiful-arrangement/526-beautiful-arrangement.cpp
@@ -1,11 +1,20 @@
 class Solution {
 public:
-    int countArrangement(int n) {
- int results[12] = {8, 10, 36, 41, 132, 250, 700, 750, 4010, 4237, 10680, 24679};
-  
-        if (n < 4) return n;
-        return results[n - 4];
-    
+    int seen =0;
+    int res = 0;
+    int countArrangement(int n,int pos = 1) {
+    if(n<4) return n;
+    if(pos>n) return res++;
+    for(int i =1 ;i<=n;i++){
+     int bit = 1<<i;
+      if(!(bit&seen)&&(pos%i==0||i%pos==0)){
+          seen^=bit;
+          countArrangement(n,pos+1);
+          seen^=bit;
+      }
+    } 
+   return res;
        
     }
+    
 };
