2b325d2b52decaa84bcb07e72a59c740ce5f8385
AMAN RAJ <amanraj7166@gmail.com>
2022-11-28 17:03:43 +0530

    Time: 11 ms (47.40%), Space: 7.8 MB (74.46%) - LeetHub

--- 0338-counting-bits/0338-counting-bits.cpp
+++ 0338-counting-bits/0338-counting-bits.cpp
@@ -4,17 +4,21 @@ public:
    
     vector<int> countBits(int n) {
          
-        vector<int> ans;
+        vector<int> ans(n+1);
+        ans[0]= 0;
         
-        for(int i = 0;i<=n ;i++){
-            int sum = 0;
-            int num = i;
-            while(num!=0){
-                sum += num%2;
-                num/=2;
-            }
-            ans.push_back(sum);
-        }
+        // for(int i = 0;i<=n ;i++){
+        //     int sum = 0;
+        //     int num = i;
+        //     while(num!=0){
+        //         sum += num%2;
+        //         num/=2;
+        //     }
+        //     ans.push_back(sum);
+        // }
+        
+        
+        for(int i = 1;i<=n;i++) ans[i] = ans[i/2]+ i%2;
         return ans;
     }
 };
