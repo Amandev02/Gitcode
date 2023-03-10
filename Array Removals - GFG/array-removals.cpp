0ce3ff7bc347428ddc922e7c705c179aac34ce33
AMAN RAJ <amanraj7166@gmail.com>
2023-03-10 23:25:08 +0530

    Added solution - LeetHub

--- Array Removals - GFG/array-removals.cpp
+++ Array Removals - GFG/array-removals.cpp
@@ -10,31 +10,19 @@ using namespace std;
 
 class Solution{
     public:
-    // Function to find
-// rightmost index
-// which satisfies the condition
 // arr[j] - arr[i] <= k
    int func(int i,int j,int k, vector<int>& arr, vector<vector<int>> &dp){
         if(i>=j) return 0;
         if(arr[j]-arr[i] <=k) return 0;
         if(dp[i][j]!=-1) return dp[i][j];
-    
-         return dp[i][j] = 1 + min(func(i+1,j,k,arr,dp),func(i,j-1,k,arr,dp));
-         
-        
-        
+        return dp[i][j] = 1 + min(func(i+1,j,k,arr,dp),func(i,j-1,k,arr,dp));
     }
     int removals(vector<int>& arr, int k){
         //Code here
         sort(arr.begin(),arr.end());
         int n = arr.size();
         vector<vector<int>> dp(n+1,vector<int>(n+1,-1));
-        
-       
-        
         return func(0,n-1,k,arr,dp);
-        
-        
     }
 };
 
