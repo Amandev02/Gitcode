df5b3e67131e2ed84cea76f419a913484013f2f0
AMAN RAJ <amanraj7166@gmail.com>
2023-03-23 00:20:13 +0530

    Time: 568 ms (53.86%), Space: 15.8 MB (61.74%) - LeetHub

--- 1289-minimum-falling-path-sum-ii/1289-minimum-falling-path-sum-ii.cpp
+++ 1289-minimum-falling-path-sum-ii/1289-minimum-falling-path-sum-ii.cpp
@@ -8,17 +8,18 @@ public:
              dp[0][i]=grid[0][i];
         
         
-      for(int i=1;i<grid.size();i++){
-            for(int j=0;j<grid.size();j++){
-                int ans=INT_MAX;
-                for(int k=0;k<grid.size();k++){
+        for(int i =1;i<n;i++){
+           
+            for(int j = 0 ;j< n;j++){
+                 int ans = INT_MAX;
+                for(int k = 0;k<n;k++){
                     if(j==k) continue;
-                    ans=min(ans,grid[i][j]+dp[i-1][k]);
+                    else ans = min(ans,grid[i][j]+dp[i-1][k]);
                 }
-                dp[i][j]=ans;
+                 dp[i][j] = ans;
             }
+           
         }
-        
         int res = INT_MAX;
         for(int i =0;i<n;i++)
         res = min(res,dp[n-1][i]);
