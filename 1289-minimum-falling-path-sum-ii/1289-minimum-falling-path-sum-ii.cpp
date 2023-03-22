c3d6ea9f885f15e04327f95e5634b648f912faab
AMAN RAJ <amanraj7166@gmail.com>
2023-03-23 00:19:06 +0530

    Time: 618 ms (50.47%), Space: 15.8 MB (61.74%) - LeetHub

--- 1289-minimum-falling-path-sum-ii/1289-minimum-falling-path-sum-ii.cpp
+++ 1289-minimum-falling-path-sum-ii/1289-minimum-falling-path-sum-ii.cpp
@@ -2,13 +2,13 @@ class Solution {
 public:
     int minFallingPathSum(vector<vector<int>>& grid) {
         int n = grid.size(), m = grid[0].size();
-         vector<vector<int>> dp(grid.size(),vector<int> (grid[0].size(),-1));
-        for(int i=0;i<grid.size();i++){
-            dp[0][i]=grid[0][i];
-        }
+        vector<vector<int>> dp(n,vector<int>(m,0));
         
-        int mini=INT_MAX;
-        for(int i=1;i<grid.size();i++){
+         for(int i=0;i<n;i++)
+             dp[0][i]=grid[0][i];
+        
+        
+      for(int i=1;i<grid.size();i++){
             for(int j=0;j<grid.size();j++){
                 int ans=INT_MAX;
                 for(int k=0;k<grid.size();k++){
@@ -18,9 +18,11 @@ public:
                 dp[i][j]=ans;
             }
         }
-        for(int i=0;i<grid[0].size();i++){
-            mini=min(mini,dp[grid.size()-1][i]);
-        }
-        return mini;
+        
+        int res = INT_MAX;
+        for(int i =0;i<n;i++)
+        res = min(res,dp[n-1][i]);
+        
+        return res;
     }
 };
