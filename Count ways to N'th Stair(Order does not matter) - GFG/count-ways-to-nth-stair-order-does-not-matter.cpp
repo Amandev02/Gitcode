31df81a4f5950133a6b3c3312a739f8f8b0fa8c2
AMAN RAJ <amanraj7166@gmail.com>
2023-06-29 02:54:25 +0530

    Added solution - LeetHub

--- Count ways to N'th Stair(Order does not matter) - GFG/count-ways-to-nth-stair-order-does-not-matter.cpp
+++ Count ways to N'th Stair(Order does not matter) - GFG/count-ways-to-nth-stair-order-does-not-matter.cpp
@@ -7,15 +7,7 @@ class Solution{
 	public:
 		int nthStair(int n){
 		    //  Code here
-		    int dp[n+1] = {0};
-		    dp[0] = 0;
-		    dp[1] = 1;
-		    dp[2] = 2;
-		    
-		    for(int i = 3;i<=n;i++){
-		        dp[i] = min(dp[i-1],dp[i-2])+1;
-		    }
-		    return dp[n];
+		  return n/2 + 1;
 		    
 		}
 };
