c39e6b0f83bc0ce0f3e3754e3998d856133913cb
AMAN RAJ <amanraj7166@gmail.com>
2023-07-03 17:21:01 +0530

    Time: 0 ms (100.00%), Space: 6.5 MB (21.72%) - LeetHub

--- 0062-unique-paths/0062-unique-paths.cpp
+++ 0062-unique-paths/0062-unique-paths.cpp
@@ -9,7 +9,18 @@ public:
         return dp[m][n] = ans;
     }
     int uniquePaths(int m, int n) {
-        vector<vector<int>> dp(m+1,vector<int>(n+1,-1));
-        return fun(m-1,n-1,dp);
+        vector<vector<int>> dp(m+1,vector<int>(n+1,0));
+        dp[0][0] = 1;
+        for(int r = 0;r<m;r++){
+            for(int c = 0;c<n;c++){
+                
+                if(r==0||c==0) dp[r][c] = 1;
+                else  dp[r][c] =  dp[r-1][c]+ dp[r][c-1];
+                
+                
+                
+            }
+        }
+        return dp[m-1][n-1];
     }
 };
